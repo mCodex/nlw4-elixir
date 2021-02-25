a07a808735604fd5eb9d2064ed963bf99c72b391
Mateus Andrade <mat.andrade@live.com>
2021-02-25 17:21:21 -0300

    Added deposit view and withdraw logic

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -11,7 +11,14 @@ defmodule RocketpayWeb.AccountsController do
     |> put_status(:ok)
     |> render("update.json", account: account)
   end
+ end
 
+ def withdraw(conn, params) do
+  with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
+    conn
+    |> put_status(:ok)
+    |> render("update.json", account: account)
+  end
  end
 
 end
